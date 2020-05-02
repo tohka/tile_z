b6c42bf4a0909203bf2abd33d300d06f19f898b8
tohka <ToNaokiArita@gmail.com>
2020-05-02 16:04:01 +0900

    Create generate_tiles.rb

---
+++ generate_tiles.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+
+(0 .. 20).each do |z|
+	cmd = "convert -size 256x256 xc:transparent -stroke \"#ff0000\" " +
+    "-strokewidth 1 -draw \"line 0,0 255,0\" -draw \"line 255,0 255,255\" " +
+    "-draw \"line 255,255 0,255\" -draw \"line 0,255 0,0\" -font Tahoma " +
+    "-pointsize 16 -fill \"#ff0000\" -stroke none -draw \"text 4,250 'ZL=#{z}'\" #{z}.png"
+	`#{cmd}`
+end
